f26a53d5999b7e59668cb429d47eb847aafcd795
Asmit Kumar <114564653+Asmit-Kumar0@users.noreply.github.com>
2022-10-02 16:48:15 +0530

    Update matix_addition.cpp

--- matix_addition.cpp
+++ matix_addition.cpp
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
     int n,m;
     cin>>n>>m;
     int arr[n][m], arr1[n][m], arr2[n][m];
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=0; j<m; j++)
         {
             cin>>arr[i][j];
         }
     }
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=0; j<m; j++)
         {
             cin>>arr1[i][j];
         }
     }
-
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=0; j<m; j++)
         {
             arr2[i][j]=arr[i][j]+arr1[i][j];
         }
     }
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=0; j<m; j++)
         {
             cout<<arr2[i][j]<<" ";
         }
